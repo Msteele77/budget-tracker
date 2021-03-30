ebb7193e286a21de9ed55545990ec0bcec856c1a
Macheya Steele <mcheysteele@gmail.com>
2021-03-30 13:05:47 -0600

    updated service worker file, added manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,29 +1,29 @@
-const APP_PREFIX = 'Budget-';     
+const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js", 
-    "idb.js",
-    "/styles.css"
+  './index.html',
+	'./css/styles.css',
+	'./js/index.js',
+	'./js/idb.js',
+	'./manifest.json'
 ];
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-      })
-    )
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
 })
 
 // cache resources
@@ -32,28 +32,29 @@ self.addEventListener('install', function (e) {
         caches.open(CACHE_NAME).then(function (cache) {
           console.log('installing cache : ' + CACHE_NAME)
           return cache.addAll(FILES_TO_CACHE)
+          
         })
       )
 })
 
-// Delete outdated caches
-//self.addEventListener('activate', function (e) {
-  //  e.waitUntil(
-    //  caches.keys().then(function (keyList) {
-        // `keyList` contains all cache names under your username.github.io
-        // filter out ones that has this app prefix to create keeplist
-  //      let cacheKeeplist = keyList.filter(function (key) {
-  //        return key.indexOf(APP_PREFIX);
-    //    })
-        // add current cache name to keeplist
-      //  cacheKeeplist.push(CACHE_NAME);
+//Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+      caches.keys().then(function (keyList) {
+        
+   let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        })
+         
+        cacheKeeplist.push(CACHE_NAME);
   
-      //  return Promise.all(keyList.map(function (key, i) {
-       //   if (cacheKeeplist.indexOf(key) === -1) {
-     //       console.log('deleting cache : ' + keyList[i] );
-       //     return caches.delete(keyList[i]);
-      //    }
-   //     }));
-  //    })
- //   );
- // });
+        return Promise.all(keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] );
+            return caches.delete(keyList[i]);
+          }
+        }));
+      })
+    );
+});
+
